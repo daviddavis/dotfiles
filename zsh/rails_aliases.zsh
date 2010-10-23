d6fe2e558f089a1edb7caa92e58a691f7c06a9e0
David Davis <davidd@scimedsolutions.com>
2010-10-23 08:31:31 -0400

    More cool stuff

---
+++ zsh/rails_aliases.zsh
@@ -0,0 +1,27 @@
+# rails 3 shortcut 'r'
+alias r='rails'
+
+# launching console/server
+sc () {
+  if [ -f ./script/rails ]; then 
+    rails c $@
+  else
+    ./script/console $@
+  fi
+}
+
+sg () {
+  if [ -f ./script/rails ]; then
+    rails g $@
+  else
+    ./script/generate $@
+  fi
+}
+
+ss () {
+  if [ -f ./script/rails ]; then 
+    rails s $@
+  else
+    ./script/server $@
+  fi
+}
