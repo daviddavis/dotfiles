193f2233680d5f15482a30084a9b83277e622ded
David Davis <davidd@scimedsolutions.com>
2010-10-28 18:02:34 -0400

    Added cabal stuff and broke out path into its own file

---
+++ zsh/path.zsh
@@ -0,0 +1,4 @@
+# stuff to edit the path
+export PATH="/usr/local/bin:/usr/local/sbin:/usr/local/mysql/bin:$PATH"
+[[ -s "$HOME/.rvm/scripts/rvm" ]] && source "$HOME/.rvm/scripts/rvm"  # This loads RVM into a shell session.
+[[ -s "$HOME/.cabal" ]] && export PATH="$HOME/.cabal/bin:$PATH"
