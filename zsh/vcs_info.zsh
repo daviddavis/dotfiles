a43de5bff06c9b7b0f037f3ced0f27664813561d
David Davis <davidd@scimedsolutions.com>
2010-11-06 19:48:41 -0400

    Tweaked colors in the vcs info display

--- zsh/vcs_info.zsh
+++ zsh/vcs_info.zsh
@@ -1,10 +1,10 @@
 setopt prompt_subst
 autoload -Uz vcs_info
 zstyle ':vcs_info:*' actionformats \
-    '%F{5}(%f%s%F{5})%F{3}-%F{5}[%F{2}%b%F{3}|%F{1}%a%F{5}]%f '
+    '%F{2}[%F{3}%s%F{2}:%F{7}%b%F{2}|%F{6}%a%F{2}]%f '
 zstyle ':vcs_info:*' formats       \
-    '%F{5}(%f%s%F{5})%F{3}-%F{5}[%F{2}%b%F{5}]%f '
-zstyle ':vcs_info:(sv[nk]|bzr):*' branchformat '%b%F{1}:%F{3}%r'
+    '%F{2}[%F{3}%s%F{2}:%F{7}%b%F{2}]%f '
+zstyle ':vcs_info:(sv[nk]|bzr):*' branchformat '%b%F{2}:%F{7}%r'
 
 zstyle ':vcs_info:*' enable git cvs svn
 
