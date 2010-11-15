e6bc8572de117e11a1004f2a704943525942c4e6
David Davis <davidd@scimedsolutions.com>
2010-11-15 18:35:20 -0500

    Added directory shortcuts

---
+++ zsh/dirs.zsh
@@ -0,0 +1,7 @@
+# directory shortcuts (cd ~woot)
+export pl=~/Sites/pipeline
+export rs=~/Sites/robertson_scholars
+export p=~/Sites/pando
+export sp=~/Sites/sunpay
+export st=~/Sites/sunteck_fats
+export elms=~/Sites/elms
