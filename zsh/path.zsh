14ce234953787d6d634deea2b3149e0dbf2640db
David Davis <davidd@scimedsolutions.com>
2011-01-08 16:15:40 -0500

    Tweaked rvm and added clojure-contrib path

--- zsh/path.zsh
+++ zsh/path.zsh
@@ -2,3 +2,6 @@
 export PATH="/usr/local/bin:/usr/local/sbin:/usr/local/mysql/bin:$PATH"
 [[ -s "$HOME/.rvm/scripts/rvm" ]] && source "$HOME/.rvm/scripts/rvm"  # This loads RVM into a shell session.
 [[ -s "$HOME/.cabal" ]] && export PATH="$HOME/.cabal/bin:$PATH"
+
+# classpath for clojure-contrib
+[[ -s "/usr/local/Cellar/clojure-contrib" ]] && export CLASSPATH="$CLASSPATH:/usr/local/Cellar/clojure-contrib/1.2.0/clojure-contrib.jar"
