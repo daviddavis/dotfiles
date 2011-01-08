03eb5ee8df84c45fc07d79c8f177efdfd339bf11
David Davis <davidd@scimedsolutions.com>
2011-01-08 16:15:40 -0500

    Tweaked rvm and added clojure-contrib path

--- zsh/lib/rvm_info.zsh
+++ zsh/lib/rvm_info.zsh
@@ -1,7 +1,7 @@
 # get the name of the branch we are
 function rvm_prompt_info() {
   ruby_version=$(~/.rvm/bin/rvm-prompt 2> /dev/null) || return
-  if [[ -n $ruby_version ]] && echo "%F{2}[%f$ruby_version%F{2}]%f"
+  if [[ -n $ruby_version ]] && echo "%F{2}-%F{7}$ruby_version%F{2}-%f"
 }
 
 RPROMPT+='$(rvm_prompt_info)'
