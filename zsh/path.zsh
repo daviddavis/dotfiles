96c41376df70c834a06cb52a0cc6238ae6fb552e
David Davis <davidd@scimedsolutions.com>
2011-02-08 16:02:19 -0500

    Added path for phpsh and fixed ff snippets for rails 3

--- zsh/path.zsh
+++ zsh/path.zsh
@@ -2,6 +2,7 @@
 export PATH="/usr/local/bin:/usr/local/sbin:/usr/local/mysql/bin:$PATH"
 [[ -s "$HOME/.rvm/scripts/rvm" ]] && source "$HOME/.rvm/scripts/rvm"  # This loads RVM into a shell session.
 [[ -s "$HOME/.cabal" ]] && export PATH="$HOME/.cabal/bin:$PATH"
+[[ -s "/usr/local/Cellar/python/2.7/bin" ]] && export PATH="/usr/local/Cellar/python/2.7/bin:$PATH"
 
 # classpath for clojure-contrib
 [[ -s "/usr/local/Cellar/clojure-contrib" ]] && export CLASSPATH="$CLASSPATH:/usr/local/Cellar/clojure-contrib/1.2.0/clojure-contrib.jar"
