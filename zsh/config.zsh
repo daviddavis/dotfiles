8e4f6ce966ff83e4f081c8d99f082ff0a4e45be4
David Davis <davidd@scimedsolutions.com>
2012-09-21 18:12:05 -0400

    Remove nicklist due to bugs

--- zsh/config.zsh
+++ zsh/config.zsh
@@ -25,30 +25,3 @@ export NODE_PATH="$NODE_PATH:/usr/local/lib/node_modules:/usr/local/lib/node"
 #bindkey '^[[3~' delete-char
 bindkey  '^[^N' tab
 #bindkey '^?' backward-delete-char 
-
-
-# add some irssi functions
-
-# create the pane with irssi's nicklist
-function irssi_nickpane() {
-    tmux renamew irssi                                              # name the window
-    tmux -q setw main-pane-width $(( $(tput cols) - 21))            # set the main pane width to the total width-20
-    tmux splitw -v "cat ~/.irssi/nicklistfifo"                      # create the window and begin reading the fifo
-    tmux -q selectl main-vertical                                   # assign the layout
-    tmux selectw -t irssi                                           # select window 'irssi'
-    tmux selectp -t 1                                               # select pane 0
-}
-
-# irssi wrapper
-function irssi() {
-    irssi_nickpane
-    $(which irssi)                                                  # launch irssi
-}
-
-# repair running irssi's nicklist pane
-function irssi_repair() {
-    tmux selectw -t irssi
-    tmux selectp -t 1
-    tmux killp -a                                                   # kill all panes
-    irssi_nickpane
-}
