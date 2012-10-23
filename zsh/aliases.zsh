9b09f429f23b434b2bd699ac71c57513b1a91dfa
David Davis <daviddavis@redhat.com>
2012-10-23 14:47:41 -0400

    Refactored aliases and added pry alias

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -1,15 +1,7 @@
 alias ll="ls -alh"
-#alias s="script/server -u"
-#alias sc="script/console -u"
-alias sc="rc"
 alias -g ....='../../..'
 alias -g .....='../../../..'
-alias m="mux"
-alias u="unicorn_rails"
-alias s="screen"
 alias clr="lein repl" # clojure lein repl
-alias b="bundle"
-alias heroku="nocorrect bundled_heroku"
 
 alias tag="ctags -R --exclude=.git --exclude=log --exclude='*.js'"
 
@@ -25,17 +17,19 @@ alias gd="git diff"
 alias cuke="bundle exec cucumber --require features"
 alias wip="bundle exec rake cucumber:wip"
 
-#alias gpull='git pull origin `get_git_branch`'
-#alias gpush='git push origin `get_git_branch`'
-
 alias fact="elinks -dump randomfunfacts.com | sed -n '/^| /p' | tr -d \|"
 
+# ruby aliases
 alias rvm="nocorrect rvm"
 alias heroku="nocorrect heroku"
+alias b="bundle"
+alias u="unicorn_rails"
+alias prc="pry -r config/environment.rb"
 
 # fix colors in tmux on gnome terminal
 alias tmux="TERM=xterm-256color tmux"
 alias mux="TERM=xterm-256color mux"
+alias m="mux"
 
 alias wee="weechat-curses"
 alias ov="sudo openvpn /etc/openvpn/*.conf"
