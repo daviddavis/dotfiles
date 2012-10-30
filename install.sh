d6ae6ffdbbcdb211173b392ef1572c219f0a25be
David Davis <daviddavis@redhat.com>
2012-10-30 12:57:32 -0400

    Adding install script

---
+++ install.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+#      Dotfiles install script
+# To install, just run the following command. You must have ruby, rake, git,
+# and zsh installed.
+#
+# curl -L https://raw.github.com/daviddavis/dotfiles/master/install.sh | sh
+#
+
+# install oh-my-zsh
+curl -L https://github.com/robbyrussell/oh-my-zsh/raw/master/tools/install.sh | sh
+
+# get the dotfiles
+git clone --recursive git@github.com:daviddavis/dotfiles.git ~/.dotfiles
+
+# install vim plugins
+cd ~/.dotfiles
+rake
+vim -c "BundleInstall"
