43f8d7694bcfa51e0607aa42949bbf1e1c638e0f
David Davis <daviddavis@redhat.com>
2013-01-10 13:44:04 -0500

    Added git upstream commands

--- zsh/katello.zsh
+++ zsh/katello.zsh
@@ -20,36 +20,3 @@ function kpr() {
     hub pull-request -b katello:`current_branch`
   fi
 }
-
-function kr() {
-  if [[ -n $1 ]]
-  then
-    br=$1
-  else
-    br=`current_branch`
-  fi
-
-  git fetch upstream
-  git reset upstream/$br
-}
-
-function krh() {
-  if [[ -n $1 ]]
-  then
-    br=$1
-  else
-    br=`current_branch`
-  fi
-
-  print "Fetching upstream..."
-  git fetch upstream
-  git log upstream/$br.. --max-count=5
-  echo "The above commit(s) will be lost. Press Y to continue: \c"
-  read line
-  if [ "$line" = Y ] || [ "$line" = y ]; then
-    git reset --hard upstream/$br
-  else
-    echo "Aborted!"
-    return 1
-  fi
-}
