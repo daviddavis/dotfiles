e417aa6cab016feae4e46e57286c2ec072ac5f4f
David Davis <daviddavis@redhat.com>
2013-01-11 16:13:48 -0500

    Fixed krdb alias

--- zsh/katello.zsh
+++ zsh/katello.zsh
@@ -1,5 +1,5 @@
 export KATELLO_PATH="/home/dadavis/Projects/katello"
-alias krdb="$KATELLO_PATH/src/script/katello-reset-dbs development . -f"
+alias krdb="$KATELLO_PATH/src/script/katello-reset-dbs -f development ."
 alias kping="katello ping"
 alias k="~/.bin/katello"
 alias kpl="pylint --rcfile=/etc/spacewalk-pylint.rc --additional-builtins=_ katello"
