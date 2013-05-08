4105362353b6f1093054e48fd3f2fbf30e3301d1
David Davis <daviddavis@redhat.com>
2013-05-08 13:15:48 -0400

    Updating KATELLO_PATH and vundle

--- zsh/katello.zsh
+++ zsh/katello.zsh
@@ -1,16 +1,16 @@
-[[ -n $KATELLO_PATH ]] || export KATELLO_PATH="/home/dadavis/Projects"
+[[ -n $KATELLO_PATH ]] || export KATELLO_PATH="/home/dadavis/Projects/katello"
 
 # directories
 export se=~s/system-engine
-export ks="$KATELLO_PATH/katello"
-export kc="$KATELLO_PATH/katello-cli"
+export ks="$KATELLO_PATH"
+export kc="${KATELLO_PATH}-cli"
 
 # aliases
 alias krdb="$ks/script/delayed_job stop && $ks/script/katello-reset-dbs -f development ."
 alias kping="katello ping"
 alias k="~/.bin/katello"
 alias kpl="pylint --rcfile=/etc/spacewalk-pylint.rc --additional-builtins=_ katello"
-alias dj="$KATELLO_PATH/katello/script/delayed_job"
+alias dj="$KATELLO_PATH/script/delayed_job"
 alias kres="krdb && dj start && rsd"
 
 function kpr() {
