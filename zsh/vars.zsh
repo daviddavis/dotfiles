e5fe1d49e432b16e38d90621a985fbfa23773a34
David Davis <daviddavis@redhat.com>
2013-10-07 15:31:40 -0400

    rvm wants to be first in path

--- zsh/vars.zsh
+++ zsh/vars.zsh
@@ -3,12 +3,14 @@ export RAILS_RELATIVE_URL_ROOT=/katello
 
 # stuff to edit the path
 export PATH="/usr/local/bin:/usr/local/sbin:/usr/local/mysql/bin:/usr/local/Cellar/python/2.7.1/bin:$PATH"
-[[ -s "$HOME/.rvm/scripts/rvm" ]] && source "$HOME/.rvm/scripts/rvm"  # This loads RVM into a shell session.
 [[ -s "$HOME/.cabal" ]] && export PATH="$HOME/.cabal/bin:$PATH"
 [[ -s "/usr/local/Cellar/python/2.7/bin" ]] && export PATH="/usr/local/Cellar/python/2.7/bin:$PATH"
 
 # personal bin
 [[ -s "$HOME/.bin" ]] && export PATH="$HOME/.bin:$PATH"
+
+# rvm
+[[ -s "$HOME/.rvm/scripts/rvm" ]] && source "$HOME/.rvm/scripts/rvm"  # This loads RVM into a shell session.
 
 # classpath for clojure-contrib
 [[ -s "/usr/local/Cellar/clojure-contrib" ]] && export CLASSPATH="$CLASSPATH:/usr/local/Cellar/clojure-contrib/1.2.0/clojure-contrib.jar"
