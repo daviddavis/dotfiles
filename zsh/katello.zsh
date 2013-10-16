c6fad698dbc00662f81a5df0b625d6aefa808ee1
David Davis <daviddavis@redhat.com>
2013-10-16 11:23:00 -0400

    Removed functions, fixed pull regex

--- zsh/katello.zsh
+++ zsh/katello.zsh
@@ -13,48 +13,5 @@ alias kpl="pylint --rcfile=/etc/spacewalk-pylint.rc --additional-builtins=_ kate
 alias dj="$KATELLO_PATH/script/delayed_job"
 alias kres="krdb && dj start && rsd"
 
-function kpr() {
-  if [[ -n $2 ]]
-  then
-    dst_branch=$2
-  else
-    dst_branch="master"
-  fi
-
-  # push current branch
-  git push origin `current_branch`
-
-  if [[ -n $1 ]]
-  then
-    hub pull-request $1 -b katello:$dst_branch
-  else
-    hub pull-request -b katello:$dst_branch
-  fi
-}
-
-function kcp() {
-  if [[ -n $2 ]]
-  then
-    temp_branch=$2
-  else
-    temp_branch=temp/`date +%Y%m%d%H%M%S`
-  fi
-  message=$1
-  branch=`current_branch`
-
-  git checkout -b $temp_branch
-  git add `git rev-parse --show-toplevel`
-  git commit -am "$message"
-  git push origin $temp_branch
-  hub pull-request "$message" -b katello:$branch
-}
-
-function kip() {
-  temp_branch=temp_`date +%s`
-
-  git checkout -b $temp_branch
-  git add `git rev-parse --show-toplevel`
-  git commit -am "$1"
-  git push origin $temp_branch
-  hub pull-request -i $2 -b katello:$branch
-}
+alias kpr="pull"
+alias kcp="pull-commit"
