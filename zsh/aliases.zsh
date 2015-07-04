ff91b1cd1cee62588783092a9eda39b6929902df
David Davis <daviddavis@redhat.com>
2015-07-04 16:36:45 -0400

    Organizing aliases

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -3,24 +3,21 @@ alias -g ....='../../..'
 alias -g .....='../../../..'
 alias clr="lein repl" # clojure lein repl
 alias t=tmux
+alias m="mux"
+alias y="yum"
+alias wee="mux s wee"
+alias gist="gist -p" # automatically make gists private
 
 alias tag="ctags --tag-relative -Rf.git/tags --exclude=.git --exclude=log --languages=-javascript,sql"
 
 alias g="nocorrect git"
 alias gg="g st -s"
 alias gs="git stash"
-alias gpd="gist -p --type=diff"
 alias gaa="git rev-parse --show-toplevel | xargs git add -A"
-alias gtd="cd \`git rev-parse --show-toplevel\`"
+
 alias rp="rake && ggpush"
 alias rdbm="rake db:migrate"
 alias testdb="RAILS_ENV=test rake db:drop && RAILS_ENV=test rake db:create && RAILS_ENV=test rake db:migrate"
-alias gd="git diff"
-alias gcb="git checkout -b"
-alias gsu="grt; git submodule update --init; cd -"
-
-alias cuke="bundle exec cucumber --require features"
-alias wip="bundle exec rake cucumber:wip"
 
 alias fact="elinks -dump randomfunfacts.com | sed -n '/^| /p' | tr -d \|"
 
@@ -32,7 +29,3 @@ alias u="unicorn_rails"
 alias prc="pry -r config/environment.rb"
 alias rr="cd .. && cd - && rvm-prompt" # reload rvm
 
-alias m="mux"
-alias y="yum"
-alias wee="mux s wee"
-alias gist="gist -p" # automatically make gists private
