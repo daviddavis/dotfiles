08bb0841eaf83566e6fccb93b3e9f3945ee35e9a
David Davis <daviddavis@users.noreply.github.com>
2025-04-29 15:04:44 +0000

    Neovim: ensure pyright, mypy, ruff LSPs are installed

--- config/nvim/init.lua
+++ config/nvim/init.lua
@@ -607,6 +607,9 @@ require('lazy').setup({
         -- ts_ls = {},
         --
 
+        pyright = {},
+        ruff = {},
+        mypy = {},
         lua_ls = {
           -- cmd = { ... },
           -- filetypes = { ... },
@@ -868,7 +871,7 @@ require('lazy').setup({
     main = 'nvim-treesitter.configs', -- Sets main module to use for opts
     -- [[ Configure Treesitter ]] See `:help nvim-treesitter`
     opts = {
-      ensure_installed = { 'bash', 'c', 'diff', 'html', 'lua', 'luadoc', 'markdown', 'markdown_inline', 'query', 'vim', 'vimdoc' },
+      ensure_installed = { 'bash', 'c', 'diff', 'html', 'lua', 'luadoc', 'markdown', 'markdown_inline', 'query', 'vim', 'vimdoc', 'python' },
       -- Autoinstall languages that are not installed
       auto_install = true,
       highlight = {
