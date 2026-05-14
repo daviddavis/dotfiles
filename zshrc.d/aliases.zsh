012ad3384de47ea9fc89bd13218fdadedb7fe7e4
David Davis <daviddavis@users.noreply.github.com>
2026-05-14 17:48:39 -0400

    Convert svim alias to function

--- zshrc.d/aliases.zsh
+++ zshrc.d/aliases.zsh
@@ -10,4 +10,4 @@ alias gg="g st -s"
 alias gs="git stash"
 alias gaa="git rev-parse --show-toplevel | xargs git add -A"
 
-alias svim='sudo -E HOME="$HOME" "$(which nvim)"'
+svim() { sudo -E HOME="$HOME" "$(command -v nvim)" "$@"; }
